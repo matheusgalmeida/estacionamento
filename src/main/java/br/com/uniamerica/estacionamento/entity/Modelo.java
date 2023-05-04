1d1a8f451d78baba40427f33f07fd039e6f3b944
Matheus Almeida <almeida.tmatheus@gmail.com>
2023-05-04 19:41:36 -0300

    Up

--- src/main/java/br/com/uniamerica/estacionamento/entity/Modelo.java
+++ src/main/java/br/com/uniamerica/estacionamento/entity/Modelo.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 @Entity
 @Table(name = "modelos", schema = "public")
-public class Modelo extends AbstractEntity {
+public class  Modelo extends AbstractEntity {
     @Getter @Setter
     @Column(name = "nome", nullable = false, unique = true)
     private String nome;
